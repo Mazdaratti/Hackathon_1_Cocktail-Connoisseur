11efe71966be86947bac01fc05a288aa46ccc50f
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-09-18 13:34:17 +0200

    cocktails.py updated

---
+++ cocktails.py
@@ -0,0 +1,128 @@
+cocktails = {
+    "easy": [
+        {
+            "name": "Margarita",
+            "wikipedia": "Margarita"
+        },
+        {
+            "name": "Mojito",
+            "wikipedia": "Mojito"
+        },
+        {
+            "name": "Tequila Sunrise",
+            "wikipedia": "Tequila_Sunrise_(cocktail)"
+        },
+        {
+            "name": "Old Fashioned",
+            "wikipedia": "Old_fashioned_(cocktail)"
+        },
+        {
+            "name": "Cosmopolitan",
+            "wikipedia": "Cosmopolitan_(cocktail)"
+        },
+        {
+            "name": "Bloody Mary",
+            "wikipedia": "Bloody_Mary_(cocktail)"
+        },
+        {
+            "name": "Daiquiri",
+            "wikipedia": "Daiquiri"
+        },
+        {
+            "name": "Manhattan",
+            "wikipedia": "Manhattan_(cocktail)"
+        },
+        {
+            "name": "Black Russian",
+            "wikipedia": "Black_Russian"
+        },
+        {
+            "name": "Whiskey Sour",
+            "wikipedia": "Whiskey_sour"
+        }
+    ],
+    "medium": [
+        {
+            "name": "Negroni",
+            "wikipedia": "Negroni"
+        },
+        {
+            "name": "Mai Tai",
+            "wikipedia": "Mai_Tai"
+        },
+        {
+            "name": "Caipirinha",
+            "wikipedia": "Caipirinha"
+        },
+        {
+            "name": "Paloma",
+            "wikipedia": "Paloma_(cocktail)"
+        },
+        {
+            "name": "Pina Colada",
+            "wikipedia": "Piña_Colada"
+        },
+        {
+            "name": "Sex on the Beach",
+            "wikipedia": "Sex_on_the_Beach"
+        },
+        {
+            "name": "Sidecar",
+            "wikipedia": "Sidecar_(cocktail)"
+        },
+        {
+            "name": "Mimosa",
+            "wikipedia": "Mimosa_(cocktail)"
+        },
+        {
+            "name": "Espresso Martini",
+            "wikipedia": "Espresso_Martini"
+        },
+        {
+            "name": "Blue Hawaii",
+            "wikipedia": "Blue_Hawaii_(cocktail)"
+        }
+    ],
+    "hard": [
+        {
+            "name": "Paradise",
+            "wikipedia": "Paradise_(cocktail)"
+        },
+        {
+            "name": "Brandy Crusta",
+            "wikipedia": "Brandy_Crusta"
+        },
+        {
+            "name": "Pisco Sour",
+            "wikipedia": "Pisco_sour"
+        },
+        {
+            "name": "Bijou",
+            "wikipedia": "Bijou_(cocktail)"
+        },
+        {
+            "name": "Paper Plane",
+            "wikipedia": "Paper_Plane_(cocktail)"
+        },
+        {
+            "name": "Hanky Panky",
+            "wikipedia": "Hanky_Panky_(cocktail)"
+        },
+        {
+            "name": "Aviation",
+            "wikipedia": "Aviation_(cocktail)"
+        },
+        {
+            "name": "Pink Lady",
+            "wikipedia": "Pink_Lady_(cocktail)"
+        },
+        {
+            "name": "Tom Collins",
+            "wikipedia": "Tom_Collins"
+        },
+        {
+            "name": "Monaco",
+            "wikipedia": "Monaco_(cocktail)"
+        }
+    ]
+}
