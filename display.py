4ffa40cc0bf920f071707dd5b037fba4fd6fc2ae
Mazdaratti <vasya.mazdaratti@googlemail.com>
2024-09-18 13:08:11 +0200

    Update display.py
    Description and docstrings added

--- display.py
+++ display.py
@@ -1,9 +1,30 @@
+"""
+Display Module
 
+This module contains functions to handle the display of game instructions and the winner celebration in the 'Cocktail Connoisseur' game. The functions are designed to enhance the user experience by providing colorful, formatted messages using the `colorama` library.
+
+Functions:
+- display_game_instructions(): Displays the game rules and instructions.
+- celebrate_winner(players_scores): Celebrates the winner(s) by displaying a special message based on the game results.
+"""
 
 from colorama import Fore, Style
 
 
 def display_game_instructions():
+    """
+        Displays the game instructions for 'Cocktail Connoisseur'.
+
+        This function prints out detailed instructions for playing the game,
+        including how to play, the objective, scoring system, and useful tips
+        for maximizing scores. The instructions are formatted using colored text
+        to enhance readability and engagement.
+
+        The instructions include:
+         - The goal of the game.
+        - How the scoring works.
+        - Tips on when to use hints.
+      """
     print(f"""
 {Fore.BLUE}==================================================
      🍸 Welcome to *Cocktail Connoisseur* 🍸
@@ -32,6 +53,22 @@ Let's get started!
 
 
 def celebrate_winner(players_scores):
+    """
+        Celebrates the winner(s) of the 'Cocktail Connoisseur' game.
+
+        This function takes the player scores and finds the player(s) with the highest score.
+        It then displays a celebratory message based on the results:
+        - If there's one winner, it congratulates them.
+        - If there's a tie, it congratulates all players involved.
+        - If a player scored zero points, a motivational message is displayed.
+
+        Args:
+            players_scores (dict): A dictionary where the keys are player names
+                                 and the values are their corresponding scores.
+
+        Returns:
+            None
+      """
     # Find the player(s) with the highest score
     max_score = max(players_scores.values())
     winners = [player for player, score in players_scores.items() if score == max_score]
